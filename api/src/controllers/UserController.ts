19c8c5ebe72efd921fdca7fc476820c1d87a1d93
Matheus Poli <matheuspolicamilo@gmail.com>
2021-03-21 00:05:32 -0300

    WIP Users CRUD

--- api/src/controllers/UserController.ts
+++ api/src/controllers/UserController.ts
@@ -9,10 +9,79 @@ import { Request, Response } from 'express';
 //   client.close();
 // });
 
+enum Genre {
+  male,
+  female,
+  other
+}
+
+enum Role {
+  top,
+  jungle,
+  mid,
+  adCarry,
+  support,
+  coach,
+  analyst,
+  caster,
+  commentator
+}
+
+interface User {
+  id: number,
+  firstName: string,
+  lastName: string,
+  genre: Genre,
+  birthDate: Date,
+  email: string,
+  whatsapp: string,
+  twitter: string,
+  twitch: string,
+  instagram: string,
+  facebook: string,
+  lolNickname: string,
+  role: Role
+}
+
+const users: Array<User> = []
+
 class UserController {
+  async index(request: Request, response: Response) {
+    return response.json(users)
+  }
+
+  async new(request: Request, response: Response) {
+    const user: User = {
+      id: null,
+      firstName: '',
+      lastName: '',
+      genre: 0,
+      birthDate: new Date(),
+      email: '',
+      whatsapp: '',
+      twitter: '',
+      twitch: '',
+      instagram: '',
+      facebook: '',
+      lolNickname: '',
+      role: 0
+    }
+
+    return response.json(user)
+  }
+
   async show(request: Request, response: Response) {
-    console.log(request.body)
-    return response.json([{id: 1, name: 'First user'}])
+    const userId = parseInt(request.params.userId)
+    response.json(users.find(user => user.id === userId))
+  }
+
+  async create(request: Request, response: Response) {
+    const user = request.body.user;
+    users.push(user);
+    response.json('Usuário criado com sucesso!')
+  }
+
+  async update(request: Request, response: Response) {
   }
 }
 
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -5,6 +5,10 @@ const router = Router()
 
 const userController = new UserController()
 
-router.get("/users", userController.show)
+router.get("/api/users", userController.index)
+router.get("/api/users/new", userController.new)
+router.post("/api/users", userController.create)
+router.get("/api/users/:userId(\\d+)", userController.show)
+router.post("/api/users/:userId(\\d+)", userController.update)
 
 export { router }
