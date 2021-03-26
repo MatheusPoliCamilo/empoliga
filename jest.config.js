630f2b2c081bb1be908bc9c70ae48eae6bf4fb85
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-03-25 22:35:39 -0300

    Fix lint

--- jest.config.js
+++ jest.config.js
@@ -2,22 +2,21 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
   roots: [
-    "<rootDir>/__tests__"
+    '<rootDir>/__tests__'
   ],
-  testEnvironment: "node",
   testPathIgnorePatterns: [
-    "/node_modules/",
+    '/node_modules/'
   ],
   globals: {
     // we must specify a custom tsconfig for tests because we need the typescript transform
     // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
     // can see this setting in tsconfig.jest.json -> "jsx": "react"
     // https://github.com/vercel/next.js/issues/8663#issue-490553899
-    "ts-jest": {
-      tsconfig: "tsconfig.test.json"
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
     }
   }
-};
+}
