5a64e484d4c80b2fcbfdb60b9ad23b35bb7deb76
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-04-02 23:12:47 -0300

    Add Bulma to the project

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import '../node_modules/bulma/bulma.sass'
+
+export default function MyApp ({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import Head from 'next/head'
+
 export default function Index () {
   return (
     <div>
-        <p>Hello Next.js</p>
-      </div>
+      <Head>
+        <title>Empoliga</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* <link rel="stylesheet" href="../css/index.css"></link> */}
+      </Head>
+
+      <a className="button is-primary">
+        Button
+      </a>
+      <p>Hello Next.js</p>
+    </div>
   )
 }
