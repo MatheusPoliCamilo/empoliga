d0f7be1009a37aa9ea44f85da325178d9f79d8f3
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-04-04 00:58:58 -0300

    Create User put

--- pages/api/users/[userId].ts
+++ pages/api/users/[userId].ts
@@ -48,6 +48,27 @@ export default async (request, response) => {
       })
     }
 
+    case 'PUT': {
+      const { userId } = request.query
+      const condition = { _id: userId }
+      const update = request.body
+      const options = { new: true }
+
+      return User.findOneAndReplace(condition, update, options, (error, user) => {
+        database.close()
+
+        if (error) {
+          return response.status(500).json(error)
+        }
+
+        if (!user) {
+          return response.status(422).json({ erros: { message: 'Usuário não encontrado' } })
+        }
+
+        return response.status(200).json(user)
+      })
+    }
+
     default: {
       database.close()
       return response.status(404).json({})
