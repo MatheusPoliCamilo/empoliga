85de94f4d86dca6d84a892ffb6515a13326de939
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-04-04 16:07:03 -0300

    WIP Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
-// import '../node_modules/bulma/bulma.sass'
 import '../styles/index.scss'
 
-export default function MyApp ({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,79 +1,89 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react'
 
-export default function Index () {
+export default function Index() {
   return (
-    <div>
+    <div className='has-text-weight-bold'>
       <Head>
         <title>Empoliga</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
 
-      <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://empoliga.vercel.app">
-            <Image src="/logo.png" width={100} height={100} alt="Logo da Empoliga" />
-          </a>
-
-          <span style={{ borderLeft: '2px solid #7E7E7E', paddingTop: '1rem', paddingBottom: '1rem' }}></span>
-
-          <Image src="/lol.png" width={100} height={100} alt="Logo da Empoliga" />
-
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
-        </div>
-
-        <div id="navbar" className="navbar-menu">
-          <div className="navbar-start">
-          <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">
-                Ligas
-              </a>
-
-              <div className="navbar-dropdown">
-                <a className="navbar-item">
-                  Minor
-                </a>
-                <a className="navbar-item">
-                  Tradicional
-                </a>
-                <a className="navbar-item">
-                  Major
-                </a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">
-                  Wild Rift
-                </a>
-              </div>
-            </div>
-
-            <a className="navbar-item">
-              Equipes
-            </a>
-
-            <a className="navbar-item">
-              Jogadores
-            </a>
-          </div>
-
-          <div className="navbar-end">
-            <div className="navbar-item">
-              <div className="buttons">
-                <a className="button is-light">
-                  Login
-                </a>
-                <a className="button is-primary">
-                  <strong>Cadastre-se</strong>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
     </div>
+  )
+}
+
+function Navbar() {
+  const [active, setActive] = useState(false)
+
+  return (
+    <nav className='navbar is-dark' role='navigation' aria-label='main navigation'>
+      <div className='navbar-brand ml-6 mr-6'>
+        <a href='https://empoliga.vercel.app' className='pt-1'>
+          <Image src='/logo.svg' width={75} height={70} alt='Logo da Empoliga' />
+        </a>
+
+        <span
+          className='ml-5 mr-5 mt-5'
+          style={{
+            borderLeft: '2px solid #3C3C3C',
+            paddingTop: '1rem',
+            paddingBottom: '1rem',
+            maxHeight: '2rem',
+          }}
+        />
+
+        <Image src='/lol.svg' width={45} height={45} alt='Logo da Empoliga' />
+
+        <a
+          role='button'
+          className={active ? 'navbar-burger is-active' : 'navbar-burger'}
+          aria-label='menu'
+          aria-expanded='false'
+          data-target='empoliga-navbar'
+          onClick={() => setActive(!active)}
+        >
+          <span aria-hidden='true' />
+          <span aria-hidden='true' />
+          <span aria-hidden='true' />
+        </a>
+      </div>
+
+      <div id='empoliga-navbar' className={active ? 'navbar-menu' : 'navbar-menu is-active'}>
+        <div className='navbar-start'>
+          <div className='navbar-item has-dropdown is-hoverable'>
+            <a className='navbar-link pl-5'>Ligas</a>
+
+            <div className='navbar-dropdown'>
+              <a className='navbar-item'>Minor</a>
+              <a className='navbar-item'>Tradicional</a>
+              <a className='navbar-item'>Major</a>
+              <hr className='navbar-divider' />
+              <a className='navbar-item has-text-grey-light' style={{ pointerEvents: 'none' }}>
+                Wild Rift
+              </a>
+            </div>
+          </div>
+
+          <a className='navbar-item is-justify-content-center pl-5 pr-5'>Equipes</a>
+
+          <a className='navbar-item pl-5 pr-5'>Jogadores</a>
+        </div>
+
+        <div className='navbar-end mr-6'>
+          <div className='navbar-item'>
+            <div className='buttons'>
+              <a className='button is-text has-text-white pl-5 pr-5'>Login</a>
+              <a className='button is-primary'>
+                <strong>Cadastre-se</strong>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
