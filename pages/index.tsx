092528915c8402f715d7268941d285daa7c134f0
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-04-04 21:57:17 -0300

    WIP home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 
 export default function Index() {
   return (
-    <div className='has-text-weight-bold'>
+    <body className='has-navbar-fixed-top has-text-weight-bold'>
       <Head>
         <title>Empoliga</title>
         <meta charSet='utf-8' />
@@ -12,7 +12,32 @@ export default function Index() {
       </Head>
 
       <Navbar />
-    </div>
+
+      <div className='section is-large has-background-black' />
+
+      <div
+        className='pt-6'
+        // style={{ height: '43rem', background: 'repeating-linear-gradient(45deg, #fff 4px, #fff 20px, #e8d7be 23px)' }}
+        style={{
+          height: '50rem',
+          background: 'repeating-linear-gradient(45deg, transparent 0 20px, #e8d7be 0px 23px)',
+        }}
+      >
+        <div className='pt-6 is-flex is-justify-content-center'>
+          <Image src='/logo.svg' width={200} height={200} alt='Logo da Empoliga' />
+        </div>
+
+        <div className='pt-5 is-flex is-justify-content-space-around'>
+          <span style={{ height: '6rem', border: '2px solid #b27826' }} />
+        </div>
+
+        <div className='pt-5 is-flex is-justify-content-space-around'>
+          <Image src='/logo.svg' width={200} height={200} alt='Logo da Empoliga' />
+          <Image src='/tradicional.svg' width={200} height={200} alt='Logo da Empoliga' />
+          <Image src='/major.svg' width={200} height={200} alt='Logo da Empoliga' />
+        </div>
+      </div>
+    </body>
   )
 }
 
@@ -20,8 +45,8 @@ function Navbar() {
   const [active, setActive] = useState(false)
 
   return (
-    <nav className='navbar is-dark' role='navigation' aria-label='main navigation'>
-      <div className='navbar-brand ml-6 mr-6'>
+    <nav className='navbar is-fixed-top is-dark' role='navigation' aria-label='main navigation'>
+      <div className='navbar-brand ml-5 mr-5'>
         <a href='https://empoliga.vercel.app' className='pt-1'>
           <Image src='/logo.svg' width={75} height={70} alt='Logo da Empoliga' />
         </a>
@@ -52,7 +77,7 @@ function Navbar() {
         </a>
       </div>
 
-      <div id='empoliga-navbar' className={active ? 'navbar-menu' : 'navbar-menu is-active'}>
+      <div id='empoliga-navbar' className={active ? 'navbar-menu is-active' : 'navbar-menu'}>
         <div className='navbar-start'>
           <div className='navbar-item has-dropdown is-hoverable'>
             <a className='navbar-link pl-5'>Ligas</a>
