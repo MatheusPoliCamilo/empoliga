47a39a385b3c50ebc2746e4e34baff56ac4c6844
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-04-12 20:54:29 -0300

    WIP Create account page

--- pages/cadastrar/index.tsx
+++ pages/cadastrar/index.tsx
@@ -4,7 +4,14 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <div className='has-background-dark has-text-weight-bold' style={{ height: '100vh' }}>
-        <Image src='/lol.svg' width={45} height={45} alt='Logo da Empoliga' />
+        <div className='columns'>
+          <div className='column is-flex'>
+            <Image src='/lol.svg' width={100} height={100} alt='Logo da Empoliga' />
+            <h1 className='is-size-1 is-align-self-center ml-6' style={{ textAlign: 'center' }}>
+              Sua carreira de pro player começa agora!
+            </h1>
+          </div>
+        </div>
       </div>
     </>
   )
