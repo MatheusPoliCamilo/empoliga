b5d33d60a1481e94e18c5a0df08747011cd0e52b
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-04-11 23:57:24 -0300

    Add Empoliga logo on create user page

--- pages/cadastrar/index.tsx
+++ pages/cadastrar/index.tsx
@@ -1,7 +1,11 @@
+import Image from 'next/image'
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
-      <div className='has-background-dark has-text-weight-bold' style={{ height: '100vh' }} />
+      <div className='has-background-dark has-text-weight-bold' style={{ height: '100vh' }}>
+        <Image src='/lol.svg' width={45} height={45} alt='Logo da Empoliga' />
+      </div>
     </>
   )
 }
