1864771af386eb2b89c6e6fa7a2d14835a70d918
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-04-16 14:54:44 -0300

    Change logos and add footer to the home page

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,7 +8,7 @@ function Navbar() {
     <nav className='navbar is-fixed-top is-dark' role='navigation' aria-label='main navigation'>
       <div className='navbar-brand ml-5 mr-5'>
         <a href='/' className='pt-1 grow-on-hover'>
-          <Image src='/logo.svg' width={75} height={70} alt='Logo da Empoliga' />
+          <Image src='/tradicional.svg' width={75} height={70} alt='Logo da Empoliga' />
         </a>
 
         <span
