0fd468e499b5e98c01621ec5e324cc4390a2fdbb
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-05-17 20:06:18 -0300

    Change leagueAccount schema

--- src/schemas/leagueAccount.ts
+++ src/schemas/leagueAccount.ts
@@ -13,6 +13,21 @@ const leagueAccountSchema = new mongoose.Schema({
   puuid: {
     type: String,
   },
+  tier: {
+    type: String,
+  },
+  rank: {
+    type: String,
+  },
+  wins: {
+    type: Number,
+  },
+  losses: {
+    type: Number,
+  },
+  leaguePoints: {
+    type: Number,
+  },
 })
 
 const LeagueAccount = mongoose.models.LeagueAccount || mongoose.model('LeagueAccount', leagueAccountSchema)
