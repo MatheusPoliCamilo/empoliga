9d92b7763674eacc62e8969fea908568ac44f528
Matheus Poli Camilo <matheuspolicamilo@gmail.com>
2021-05-29 18:19:51 -0300

    Fix env config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   publicRuntimeConfig: {
     CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET,
-    CLOUDINARY_URL: process.env.CLOUDINARY_URL,
-    RIOT_API_KEY: process.env.RIOT_API_KEY,
+    CLOUDINARY_URL: process.env.CLOUDINARY_URL
   },
 }
